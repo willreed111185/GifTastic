a362f335da9c9e7ed6c18e0ad3cef29159fdb7dd
willreed111185 <william.reed.11@gmail.com>
2017-08-23 20:44:16 -0400

    proj comp

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -11,52 +11,46 @@ function renderGifBtns(){
 	}
 }
 
-
 function renderGifs(KeyWord){
-	console.log("searchTerm: ",KeyWord);
 	var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="+KeyWord+"&limit=10";
 	$.ajax({
 	    url: queryURL,
 	    method: "GET"
 	})
 	.done(function(response) {
-		console.log(response);
 		$("#imageBox").empty();
 		for(var i=0; i<response.data.length;i++){
-			var searchStill = response.data[i].images.fixed_height_still.url;
-			var searchAnimated = response.data[i].images.fixed_height.url;
-			var searchID = response.data[i].id;
-			console.log("Image: ",searchStill);
-			var searchRating = response.data[i].rating;
-			console.log("Rating: ",searchRating);
 	      	var gifImage = $("<img>");
-	      	gifImage.attr("id", searchID);
-	      	gifImage.attr("src", searchStill);
+	      	var gifRating = $("<h2>");
+	      	gifImage.attr("id", response.data[i].id);
+	      	gifImage.attr("src", response.data[i].images.fixed_height_still.url);
 	        gifImage.attr("alt", "image missing__");
-    	    gifImage.attr("data-still", searchStill);
-	       	gifImage.attr("data-animate", searchAnimated);
+    	    gifImage.attr("data-still", response.data[i].images.fixed_height_still.url);
+	       	gifImage.attr("data-animate", response.data[i].images.fixed_height.url);
 	       	gifImage.attr("data-state", "still");
 	       	gifImage.addClass("imgBtn");
-	        console.log("gifImage ",gifImage);
 	        $("#imageBox").append(gifImage);
+	        gifRating.html(response.data[i].rating)
+	        $("#imageBox").append(gifRating);
        }
 	})
+}
 
+function searchGIF(newGif){
+	gifArray.push(newGif);
+	renderGifBtns();
+	renderGifs(newGif);
 }
 
 function animateGif(id){
 	var currentState = $("#"+id).attr("data-state");
-	console.log("currentState ",currentState);
-	 if (currentState === "still") {
-        $("#"+id).attr("src", $("#"+id).attr("data-animate"));
-        $("#"+id).attr("data-state", "animate");
-      } else {
-        $("#"+id).attr("src", $("#"+id).attr("data-still"));
-        $("#"+id).attr("data-state", "still");
-      }
-
-
-
+	if (currentState === "still") {
+	   $("#"+id).attr("src", $("#"+id).attr("data-animate"));
+	   $("#"+id).attr("data-state", "animate");
+	} else {
+	   $("#"+id).attr("src", $("#"+id).attr("data-still"));
+	   $("#"+id).attr("data-state", "still");
+	}
 }
 
 $("#buttonBox").on("click",".gifButton",function(){
@@ -66,6 +60,11 @@ $("#buttonBox").on("click",".gifButton",function(){
 $("#imageBox").on("click",".imgBtn",function(){
 	animateGif(this.id);
 })
+
+$("#searchBTN").on("click", function(){
+	searchGIF($("#searchInput").val());
+})
+
 
 renderGifBtns();
 
